c6a7812327ad0405c11ffcd7df797a9265e44244
Cherry <kieran.kc.mc@gmail.com>
2026-07-22 13:57:35 +0200

    Use dotenv to load environment variables.

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+from dotenv import load_dotenv
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap
 import os
 
+load_dotenv()
+
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 Bootstrap(app)
 
 @app.route('/')
@@ -11,5 +14,5 @@ def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.getenv('PORT', 5000))
     app.run(debug=True, host="0.0.0.0", port=port)
