62b95cc410e133c219ddee3913dfd240033e326c
Cherry <kieran.kc.mc@gmail.com>
2026-08-30 18:20:06 +0200

    Add gunicorn logging

--- run.py
+++ run.py
@@ -1,13 +1,20 @@
-from dotenv import load_dotenv
+import logging
 import os
+from dotenv import load_dotenv
 
 from web import create_app
 
 load_dotenv()
 app = create_app()
 
-is_debug = os.environ.get("DEBUG", "False").lower() == "true"
+if __name__ != '__main__':
+    # Use gunicorn logging
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
 
 if __name__ == '__main__':
+    is_debug = os.environ.get("DEBUG", "False").lower() == "true"
     port = int(os.environ.get('PORT', 5000))
+
     app.run(debug=is_debug, host="0.0.0.0", port=port)
