ad9076004f50e03fd3e92868ee1c17a1672a1338
Cherry <kieran.kc.mc@gmail.com>
2026-09-14 14:17:24 +0200

    Fix requires_fresh_login requiring cookies to be cleared to log in again.

--- web/__init__.py
+++ web/__init__.py
@@ -58,6 +58,8 @@ def create_app():
     login_manager.login_view = "auth.login"
     login_manager.login_message = "Please log in to access this page."
     login_manager.login_message_category = "notice"
+    login_manager.refresh_view = "auth.login"
+    login_manager.needs_refresh_message = "Please reauthenticate to access this page."
 
     # Cloudinary configuration
     cloudinary.config(
